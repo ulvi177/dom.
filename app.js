2db7c85f1daa361734dd8bee4d74192d8837a689
ulvi177 <ulvi-abdullayev@internet.ru>
2025-07-19 21:46:37 +0000

    todoo

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 "use strict";
 $(document).ready(function () {
 
-$
+
+    $('.btn').click(function (e) {
+        e.preventDefault();
+
+        let a = (a) => {
+            for (let i = 1; i <= a; i++) {
+                $('.col-md-12').append(`<.btn>${i}</btn>`);
+            }
+
+        }
+        a($('input').val());
+    });
 
 
+
+  
 
 
 });
